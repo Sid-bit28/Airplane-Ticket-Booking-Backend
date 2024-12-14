fd15d75b8539ae3e3fed803f374df8f4c94b4b3e
Siddarth Banerjee <siddarth2801@gmail.com>
2024-12-14 23:03:33 +0530

    Added City repository and started cityrepository

--- FlightsAndSearch/src/index.js
+++ FlightsAndSearch/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
+const CityRepository = require('./repository/city-repository');
 
 const setupAndStartserver = async () => {
   const app = express();
@@ -12,6 +13,8 @@ const setupAndStartserver = async () => {
 
   app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`);
+    const repo = new CityRepository();
+    repo.createCity({ name: 'New Delhi' });
   });
 };
 
